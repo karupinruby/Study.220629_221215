cb49e2f299c901ad3607d188c5205115c69dc21d
ITWILL <ITWILL@DESKTOP-03Q3I1V>
2022-07-07 14:21:58 +0900

    sample

---
+++ 01_Hello/src/Ex01.java
@@ -0,0 +1,11 @@
+
+class Ex01 {
+
+	public static void main(String[] args) {
+		// 한 줄 주석(comment) = 사람이 보기 위한 글
+		System.out.println("Hello Java!!");
+		System.out.println("자바야 만나서 만가우이~~");
+
+	}
+
+}
---
+++ 01_Hello/src/Ex02.java
@@ -0,0 +1,77 @@
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		/*
+		abcdefg
+		1l2klfasf
+		여러 줄 주석 처리
+		 : 
+		 */
+		System.out.println("Hello World");  // 문장의 끝 세미콜론
+		System.out.println("화면 출력 문자열");  // 빨간 또는 대문자 시작 = 클래스
+		System.out.println("1234" + 1); // . = ~에 있는 / ~참조 한다
+		System.out.println(1 + "1234"); // 소문자 = 변수 또는 메소드
+		System.out.println(1234 + 9877); // 소문자 + 소괄호 = 메소드
+		
+		System.out.println();
+		System.out.println(); // Sysout + ctrl + space
+		System.out.println();
+		
+		/* 1. 콘솔창에 삼각형 모양 출력
+		 * 
+		 **
+		 ***
+		 ****
+		 *****
+		   2. 
+		     *
+		    **
+		   ***
+		  ****
+		 *****
+		   3. 구구단(2단 출력)
+		      ex) 2* 2 = 4
+		*/
+		
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+		
+		System.out.println("ㅡㅡㅡㅡㅡㅡ");
+		
+		System.out.println("    *");
+		System.out.println("   **");
+		System.out.println("  ***");
+		System.out.println(" ****");
+		System.out.println("*****");
+
+		System.out.println("ㅡㅡㅡㅡㅡㅡ");
+		
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		
+		System.out.println("ㅡㅡㅡENDㅡㅡㅡ");
+		
+		System.out.println(2 * 1);
+		System.out.println("2 * 1 = " + 2 * 1);
+		System.out.println("2 * 2 = " + 2 * 2);
+		System.out.println("2 * 3 = " + 2 * 3);
+		System.out.println("2 * 4 = " + 2 * 4);
+		System.out.println("2 * 5 = " + 2 * 5);
+		System.out.println("2 * 6 = " + 2 * 6);
+		System.out.println("2 * 7 = " + 2 * 7);
+		System.out.println("2 * 8 = " + 2 * 8);
+		System.out.println("2 * 9 = " + 2 * 9);
+		
+		System.out.println("2 * 2 = " + (2 * 9)); // 연산자 사용시 소괄호 이용시 우선 작업
+		
+		
+	}
+
+}
